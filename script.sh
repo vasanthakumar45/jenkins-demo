b007a42d405bf7572cb711002f9fc422b01fc064
vasanthakumar45 <154395432+vasanthakumar45@users.noreply.github.com>
2024-02-03 20:52:22 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # This is a simple script
-echo "Hello, World after 3 month i will get a new aws devops job!"
+echo "Hello, World after 3 month defenatly i will get a new aws devops job!"
 
